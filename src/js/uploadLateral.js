b60d39525974facdf5905329d64846bb47e3ceb1
ffsala2002-a11y <ffsala2002@gmail.com>
2026-03-13 16:03:09 -0300

    Update uploadLateral.js

--- src/js/uploadLateral.js
+++ src/js/uploadLateral.js
@@ -1,23 +1,51 @@
 export default function uploadLateral() {
 
+
+
     const btnLateral = document.getElementById('btn-lateral');
+
     const uploadLateral = document.querySelector('.menu-lateral');
+
     const fundoAtivo = document.querySelector('.fundo');
+
+    const bottomBar = document.querySelector(".bottom-bar");
+
+
 
     btnLateral.addEventListener('click', () => {
 
+
+
         uploadLateral.classList.toggle('active-lateral');
+
         fundoAtivo.classList.toggle('active-fundo');
+
         btnLateral.classList.toggle('active-lateral');
 
+        bottomBar.classList.toggle('active-bar');
+
+
+
     })
+
+
+
 
 
     fundoAtivo.addEventListener('click', () => {
 
+
+
         uploadLateral.classList.remove('active-lateral');
+
         fundoAtivo.classList.remove('active-fundo');
-         btnLateral.classList.remove('active-lateral');
+
+        btnLateral.classList.remove('active-lateral');
+
+        bottomBar.classList.remove('active-bar');
+
+
 
     })
+
 }
